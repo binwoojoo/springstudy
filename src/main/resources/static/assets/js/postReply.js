462f786b744c5573e9dbab23dd8e3131e8522d00
oooowb <oooo940212@gmail.com>
2024-05-28 12:44:06 +0900

    AJAX
    post

---
+++ src/main/resources/static/assets/js/postReply.js
@@ -0,0 +1,27 @@
+import { BASE_URL } from "./reply.js";
+import { renderReplies } from "./getReply.js";
+
+// 서버에 댓글 등록을 요청하는 비동기 함수
+export const fetchReplyPost = async () => {
+  const textInput = document.getElementById("newReplyText");
+  const writerInput = document.getElementById("newReplyWriter");
+
+  const payload = {
+    text: textInput.value,
+    author: writerInput.value,
+    bno: document.getElementById("wrap").dataset.bno,
+  };
+
+  const res = await fetch(`${BASE_URL}`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const replies = await res.json();
+
+  //   console.log(replies);
+  renderReplies(replies);
+};
